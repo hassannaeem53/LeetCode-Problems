891387f6613e95b6aa2d0906cb962bdf40e4824b
Hassan Naeem <80200407+hassannaeem53@users.noreply.github.com>
2023-01-17 18:44:13 +0500

    Time: 38 ms (81.50%), Space: 14 MB (33.99%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -6,19 +6,12 @@ class Solution:
         nums1_ind=m-1
         nums2ind=n-1
         cur=m+n-1
-        if nums1_ind==0 and nums2ind==0:
-            if nums1[0]>nums2[0]:
-                nums1[1]=nums1[0]
-                nums1[0]=nums2[0]
-                
+ 
+        while nums2ind>=0:
+            if nums1_ind>=0 and nums1[nums1_ind]>nums2[nums2ind]:
+                nums1[cur]=nums1[nums1_ind]
+                nums1_ind-=1
             else:
-                nums1[1]=nums2[0]
-        else: 
-            while nums2ind>=0:
-                if nums1_ind>=0 and nums1[nums1_ind]>nums2[nums2ind]:
-                    nums1[cur]=nums1[nums1_ind]
-                    nums1_ind-=1
-                else:
-                    nums1[cur]=nums2[nums2ind]
-                    nums2ind-=1
-                cur-=1
+                nums1[cur]=nums2[nums2ind]
+                nums2ind-=1
+            cur-=1
