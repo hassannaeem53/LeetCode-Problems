daeea14e9995b25f74e0a55dac093e584fdd971d
Hassan Naeem <80200407+hassannaeem53@users.noreply.github.com>
2023-01-18 18:30:36 +0500

    Time: 150 ms (8.80%), Space: 14.1 MB (48.56%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -2,11 +2,11 @@ class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         size1,size2=len(nums1),len(nums2)
         result=[]
-        flags=[False for x in range(len(nums1))]
-        for i in range(size2):
-            for j in range(size1):
-                if nums1[j]==nums2[i] and flags[j]==False:
-                    result.append(nums1[j])
+        flags=[False for x in range(len(nums2))]
+        for i in range(size1):
+            for j in range(size2):
+                if nums1[i]==nums2[j] and flags[j]==False:
+                    result.append(nums1[i])
                     flags[j]=True
                     break
         return result
